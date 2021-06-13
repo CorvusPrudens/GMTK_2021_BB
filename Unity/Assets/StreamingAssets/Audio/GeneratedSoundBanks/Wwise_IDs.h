9cb13c1d71a2f08252c924d01bbae5bb80ffdc08
Ben Lipkin <lipkinaudio@gmail.com>
2021-06-13 09:59:59 -0400

    Added UI SFX
    Also fixed bug movement sounds

--- Unity/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Unity/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -32,8 +32,9 @@ namespace AK
         static const AkUniqueID PRESS = 1086439346U;
         static const AkUniqueID ROLLY_DIE = 4270670118U;
         static const AkUniqueID STOPALL_LOCAL = 3853498466U;
+        static const AkUniqueID TEST = 3157003241U;
+        static const AkUniqueID WASP_BUZZ = 733302340U;
         static const AkUniqueID WASP_DIE = 994571083U;
-        static const AkUniqueID WASP_IDLE = 1051796161U;
         static const AkUniqueID WIN_STINGER = 1332089670U;
     } // namespace EVENTS
 
