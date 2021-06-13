858c1afe6c0f2749cc4876f47730054cb102ccb8
caiojmini <caiojmini@gmail.com>
2021-06-13 13:40:11 -0300

    DOORS

--- Unity/Assets/Scripts/Level/Door.cs
+++ Unity/Assets/Scripts/Level/Door.cs
@@ -13,6 +13,10 @@ public class Door : MonoBehaviour
     public Sprite brokenSprite;
     public Sprite openSprite;
 
+    private bool inRange;
+    private PlayerStats stats;
+    [SerializeField] private GameObject lockedCollision;
+
     public enum Position {
         UP,
         DOWN,
@@ -71,37 +75,49 @@ public class Door : MonoBehaviour
         switch (position)
         {
             case Position.UP:
+                rend.sprite = openSprite;
                 break;
             case Position.DOWN:
+                rend.sprite = openSprite;
                 transform.eulerAngles = new Vector3(0, 0, 180);
+                lockedCollision.SetActive(false);
                 break;
             case Position.LEFT:
+                rend.sprite = openSprite;
                 transform.eulerAngles = new Vector3(0, 0, 90);
+                lockedCollision.SetActive(false);
                 break;
             case Position.RIGHT:
+                rend.sprite = openSprite;
                 transform.eulerAngles = new Vector3(0, 0, -90);
+                lockedCollision.SetActive(false);
                 break;
 
             case Position.UP_L:
                 rend.sprite = lockedSprite;
+                lockedCollision.SetActive(true);
                 break;
             case Position.DOWN_L:
                 rend.sprite = lockedSprite;
                 transform.eulerAngles = new Vector3(0, 0, 180);
+                lockedCollision.SetActive(true);
                 break;
             case Position.LEFT_L:
                 rend.sprite = lockedSprite;
                 transform.eulerAngles = new Vector3(0, 0, 90);
+                lockedCollision.SetActive(true);
                 break;
             case Position.RIGHT_L:
                 rend.sprite = lockedSprite;
                 transform.eulerAngles = new Vector3(0, 0, -90);
+                lockedCollision.SetActive(true);
                 break;
         }
     }
 
     void Start()
     {
+        stats = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerStats>();
         SetVisuals();
     }
 
@@ -112,8 +128,52 @@ public class Door : MonoBehaviour
 
     void Update()
     {
-
+        if (inRange)
+        {
+            if (Input.GetKeyDown(KeyCode.E) && stats.keys > 0)
+            {
+                UnlockDoor();
+            }
+        }
     }
 
+    void UnlockDoor()
+    {
+        stats.keys--;
+        EventBroker.CallUpdateStatsUI();
 
+        switch (position)
+        {
+            case Position.UP_L:
+                position = Position.UP;
+                break;
+            case Position.DOWN_L:
+                position = Position.DOWN;
+                break;
+            case Position.LEFT_L:
+                position = Position.LEFT;
+                break;
+            case Position.RIGHT_L:
+                position = Position.RIGHT;
+                    break;
+        }
+        
+        SetVisuals();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            inRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            inRange = false;
+        }
+    }
 }
